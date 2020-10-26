7d07c9e3c8a2d17b7b1ce509c35b89faa80a9e51
dchengsd <dchengsd@163.com>
2020-10-26 10:16:12 +0800

    dch更新

--- src/com/dch/helloworld/HelloWorld.java
+++ src/com/dch/helloworld/HelloWorld.java
@@ -1,15 +1,34 @@
 package com.dch.helloworld;
 
+import java.util.HashMap;
+
 public class HelloWorld {
 
     public static void main(String[] args){
 
+        testHashMap();
+
         test();
+
 //        System.out.println("Hello World!");
     }
 
     public static void  test(){
         System.out.println("Hello World!!");
+
     }
+
+    public static void testHashMap(){
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("name","dch");
+        map.put("age", "12");
+        map.put("school", "UCAS");
+        map.put("major", "computer");
+
+        String name = map.get("name");
+        System.out.println(name);
+        System.out.println("name = " + name);
+    }
+    
 
 }
